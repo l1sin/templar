bb421b40c6a3476b89534743dbf9e3ace360a4c5
Andrey Lisin <andrey.lisin11@gmail.com>
2023-04-05 20:03:51 +0500

    Making rotation midpoint

--- ScrollShooter/Assets/Scripts/AimGun.cs
+++ ScrollShooter/Assets/Scripts/AimGun.cs
@@ -6,15 +6,29 @@ public class AimGun : MonoBehaviour
     [SerializeField] public static Vector3 MousePosOffset;
     [SerializeField] private Vector3 _cameraMovementScale;
     [SerializeField] private Vector3 _cameraOffset;
+    
     [SerializeField] private bool _moveCameraToCursor;
+    [SerializeField] private BodyController _bodyController;
 
     [HideInInspector] private Vector3 _mousePos;
     [HideInInspector] private Vector3 _mousePosVeiwport;
     [HideInInspector] private Vector3 _difference;
-    [HideInInspector] private float _rotationZ; 
+    [HideInInspector] private float _rotationZ;
+
+    [HideInInspector] private GameObject _activeHandL;
+    [HideInInspector] private GameObject _activeHandR;
+    [HideInInspector] public GameObject ActiveHand;
+
+    private void Start()
+    {
+        _activeHandL = _bodyController.LeftHandActiveFrontL;
+        _activeHandR = _bodyController.LeftHandActiveBackR;
+        ActiveHand = _activeHandR;
+    }
 
     private void Update()
     {
+        ChangeActiveHand();
         RotateGun();
         if (_moveCameraToCursor)
         {
@@ -25,16 +39,21 @@ public class AimGun : MonoBehaviour
     private void RotateGun()
     {
         _mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
-        _difference = _mousePos - transform.position;
-        _rotationZ = Mathf.Atan2(_difference.normalized.y, _difference.normalized.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, _rotationZ);
+        _difference = _mousePos - ActiveHand.transform.position;
+        _rotationZ = Mathf.Atan2(_difference.normalized.y, _difference.normalized.x) * Mathf.Rad2Deg; 
         if (_difference.x > 0)
         {
-            transform.rotation = Quaternion.Euler(0, 0, _rotationZ);
+            _bodyController.HeadAndBody.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+            ActiveHand = _activeHandR;
+            ActiveHand.transform.rotation = Quaternion.Euler(0f, 0f, _rotationZ);
         }
         else if (_difference.x < 0)
         {
-            transform.rotation = Quaternion.Euler(180, 0, -_rotationZ);
+            _bodyController.HeadAndBody.transform.rotation = Quaternion.Euler(0, 180, 0);
+
+            ActiveHand = _activeHandL;
+            ActiveHand.transform.rotation = Quaternion.Euler(0f, 0f, _rotationZ - 180);
         }
     }
 
@@ -52,5 +71,39 @@ public class AimGun : MonoBehaviour
         float z = ViewPortCoordinates.z;
 
         return new Vector3(x, y, z);
+    }
+
+    private void ChangeActiveHand()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            _activeHandL = _bodyController.RightHandActiveBackL;
+            _activeHandR = _bodyController.RightHandActiveFrontR;
+
+            _bodyController.LeftHandActiveBackR.SetActive(false);
+            _bodyController.LeftHandInactiveBackR.SetActive(true);
+            _bodyController.RightHandActiveFrontR.SetActive(true);
+            _bodyController.RightHandInactiveFrontR.SetActive(false);
+
+            _bodyController.LeftHandActiveFrontL.SetActive(false);
+            _bodyController.LeftHandInactiveFrontL.SetActive(true);
+            _bodyController.RightHandActiveBackL.SetActive(true);
+            _bodyController.RightHandInactiveBackL.SetActive(false);
+        }
+        else
+        {
+            _activeHandL = _bodyController.LeftHandActiveFrontL;
+            _activeHandR = _bodyController.LeftHandActiveBackR;
+
+            _bodyController.LeftHandActiveBackR.SetActive(true);
+            _bodyController.LeftHandInactiveBackR.SetActive(false);
+            _bodyController.RightHandActiveFrontR.SetActive(false);
+            _bodyController.RightHandInactiveFrontR.SetActive(true);
+
+            _bodyController.LeftHandActiveFrontL.SetActive(true);
+            _bodyController.LeftHandInactiveFrontL.SetActive(false);
+            _bodyController.RightHandActiveBackL.SetActive(false);
+            _bodyController.RightHandInactiveBackL.SetActive(true);
+        }
     }
 }
--- ScrollShooter/Assets/Scripts/Shoot.cs
+++ ScrollShooter/Assets/Scripts/Shoot.cs
@@ -2,16 +2,17 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
-    [Header("Main preferences")]
-    [SerializeField] private Transform _shootingPoint;
-
     [Header("Pistol preferences")]
+    [SerializeField] private Transform _pistolShootingPointR;
+    [SerializeField] private Transform _pistolShootingPointL;
     [SerializeField] private GameObject _pistolProjectilePrefab;
     [SerializeField] private float _pistolFirePeriod;
     [SerializeField] private float _pistolDamage;
     [SerializeField] private float _pistolProjectileSpeed;
 
-    [Header("Railgun preferences")]  
+    [Header("Railgun preferences")]
+    [SerializeField] private Transform _railgunShootingPointR;
+    [SerializeField] private Transform _railgunShootingPointL;
     [SerializeField] private GameObject _railgunProjectilePrefab;
     [SerializeField] private float _railgunFirePeriod;
     [SerializeField] private float _railgunDamage;
@@ -20,10 +21,15 @@ public class Shoot : MonoBehaviour
     
     [Header("Component references")]
     [SerializeField] private Rigidbody2D _rigidbody2D;
+    [SerializeField] private BodyController _bodyController;
+    [SerializeField] private AimGun _aimGun;
 
     [Header("Hidden")]
     [HideInInspector] private float _nextFirePistol;
     [HideInInspector] private float _nextFireRailgun;
+    [HideInInspector] private Transform _pistolShootingPoint;
+    [HideInInspector] private Transform _railgunShootingPoint;
+
 
     private void Start()
     {
@@ -33,15 +39,21 @@ public class Shoot : MonoBehaviour
 
     private void Update()
     {
+        ChangeShootingPoint();
         FirePistol();
         FireRailgun();
     }
 
     private void FirePistol()
     {
-        if (Input.GetMouseButton(0) && Time.time >= _nextFirePistol)
+        if (Input.GetMouseButton(0) && !Input.GetMouseButton(1) && Time.time >= _nextFirePistol)
         {
-            GameObject pistolProjectile = Instantiate(_pistolProjectilePrefab, _shootingPoint.position, Quaternion.Euler(transform.localEulerAngles));
+            GameObject pistolProjectile = Instantiate(_pistolProjectilePrefab, _pistolShootingPoint.position, Quaternion.Euler(_aimGun.ActiveHand.transform.localEulerAngles));
+            
+            if (!(_bodyController.HeadAndBody.transform.rotation.y == 0))
+            {
+                pistolProjectile.transform.rotation = Quaternion.Euler(_aimGun.ActiveHand.transform.localEulerAngles + new Vector3(0, 0, 180));
+            }
             PlayerProjectile projectileParameters = pistolProjectile.GetComponent<PlayerProjectile>();
             projectileParameters.Damage = _pistolDamage;
             projectileParameters.Speed = _pistolProjectileSpeed;
@@ -52,9 +64,13 @@ public class Shoot : MonoBehaviour
 
     private void FireRailgun()
     {
-        if (Input.GetMouseButton(1) && Time.time >= _nextFireRailgun)
+        if (Input.GetMouseButton(0) && Input.GetMouseButton(1) && Time.time >= _nextFireRailgun)
         {
-            GameObject railgunProjectile = Instantiate(_railgunProjectilePrefab, _shootingPoint.position, Quaternion.Euler(transform.localEulerAngles));
+            GameObject railgunProjectile = Instantiate(_railgunProjectilePrefab, _railgunShootingPoint.position, Quaternion.Euler(_aimGun.ActiveHand.transform.localEulerAngles));
+            if (!(_bodyController.HeadAndBody.transform.rotation.y == 0))
+            {
+                railgunProjectile.transform.rotation = Quaternion.Euler(_aimGun.ActiveHand.transform.localEulerAngles + new Vector3(0, 0, 180));
+            }
             PlayerProjectile projectileParameters = railgunProjectile.GetComponent<PlayerProjectile>();
             projectileParameters.Damage = _railgunDamage;
             projectileParameters.Speed = _railgunProjectileSpeed;
@@ -78,5 +94,19 @@ public class Shoot : MonoBehaviour
         }
         direction.y = Mathf.Sin(transform.localEulerAngles.z * Mathf.Deg2Rad);
         return -direction;
+    }
+
+    private void ChangeShootingPoint()
+    {
+        if (_bodyController.HeadAndBody.transform.rotation.y == 0)
+        {
+            _pistolShootingPoint = _pistolShootingPointR;
+            _railgunShootingPoint = _railgunShootingPointR;
+        }
+        else
+        {
+            _pistolShootingPoint = _pistolShootingPointL;
+            _railgunShootingPoint = _railgunShootingPointL;
+        }
     }
 }
