1fbcc5cd0267adc9fd90df5e7687a1b30b38b85c
Andrey Lisin <andrey.lisin11@gmail.com>
2023-05-02 21:36:04 +0500

    Parallax fix

--- ScrollShooter/Assets/Scripts/Parallax.cs
+++ ScrollShooter/Assets/Scripts/Parallax.cs
@@ -5,7 +5,7 @@ public class Parallax : MonoBehaviour
     [SerializeField] private Transform _parallaxAnchor;
     [HideInInspector] private Vector3 _lastAnchorPos;
     [SerializeField] private GameObject[] _parallaxObjects;
-    [SerializeField] private Vector3 _deltaXMovement;
+    [HideInInspector] private Vector3 _deltaXMovement;
     [SerializeField] private float[] _parallaxEffectivenessX;
     [SerializeField] private float[] _parallaxEffectivenessY;
     [SerializeField] private SpriteRenderer[] _renderers;
@@ -24,14 +24,19 @@ public class Parallax : MonoBehaviour
 
         for (int i = 0; i <= _parallaxObjects.Length - 1; i++)
         {
-            _parallaxObjects[i].transform.position -= new Vector3(_deltaXMovement.x, 0f, 0f) * _parallaxEffectivenessX[i];
-            _parallaxObjects[i].transform.position -= new Vector3(0f, _deltaXMovement.y, 0f) * _parallaxEffectivenessY[i];
+            _parallaxObjects[i].transform.position += new Vector3(_deltaXMovement.x, 0f, 0f) * _parallaxEffectivenessX[i];
+            _parallaxObjects[i].transform.position += new Vector3(0f, _deltaXMovement.y, 0f) * _parallaxEffectivenessY[i];
         }
+
         for (int i = 0; i <= _parallaxObjects.Length - 1; i++)
         {
             if (Mathf.Abs(_parallaxAnchor.position.x - _parallaxObjects[i].transform.position.x) > (_renderers[i].bounds.extents.x / 3 * 2))
             {
                 _parallaxObjects[i].transform.position = new Vector3(_parallaxAnchor.position.x, _parallaxObjects[i].transform.position.y, _parallaxObjects[i].transform.position.z);
+            }
+            if (Mathf.Abs(_parallaxAnchor.position.y - _parallaxObjects[i].transform.position.y) > (_renderers[i].bounds.extents.y / 3 * 2))
+            {
+                _parallaxObjects[i].transform.position = new Vector3(_parallaxObjects[i].transform.position.x, _parallaxAnchor.position.y, _parallaxObjects[i].transform.position.z);
             }
         }
 
